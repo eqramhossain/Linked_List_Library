63d815d539748ef208c1769af9bcca4b33bee726
Ekram Hossain <ekram.fr03@gmail.com>
2025-02-04 21:44:34 +0100

    updated

--- library.h
+++ library.h
@@ -6,7 +6,7 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 13:13:26 by ehossain          #+#    #+#             */
-/*   Updated: 2025/02/04 13:14:06 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/02/04 21:40:35 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,8 @@ t_node				*ft_insert_head(t_node *head, int value);
 t_node				*ft_insert_tail(t_node *head, int value);
 t_node				*ft_delete_head(t_node *head);
 t_node				*ft_delete_tail(t_node *head);
+int					ft_is_member(t_node *head, int find_value);
+int					ft_count_member(t_node *head, int find_value);
+void				ft_find_replace(t_node *head, int find_value, int replace);
+
 #endif
